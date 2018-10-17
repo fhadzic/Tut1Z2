3d10f8e177fca9facc9da91d9fa375c800bff283
fhadzic <fhadzic1@etf.unsa.ba>
2018-10-18 00:04:16 +0200

    first commit

---
+++ src/ba/unsa/etf/rpr/predavanje03/Main.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.predavanje03;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static int sumaCifara(int n){
+        int suma=0;
+        for(int i=n; i!=0; i/=10){
+            suma += i%10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+
+            Scanner skener = new Scanner(System.in);
+
+            System.out.println("Unesite broj n: ");
+
+            int n;
+
+            n= Integer.valueOf(skener.next());
+
+            System.out.println("Suma cifara broja n je: " + sumaCifara(n));
+
+
+    }
+}
